25ce70d9d34b4f73d4983ba9c38830349b0e0572
April Rivera <aprilrivera@zipcoders-MacBook-Pro-2.local>
2018-02-26 16:29:22 -0500

    just need rotate

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -1,32 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
-public class ROT13  {
+public class ROT13 {
 
     ROT13(Character cs, Character cf) {
     }
 
     ROT13() {
+
     }
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c >= 'a' && c <= 'm') c += 13;
+            else if (c >= 'A' && c <= 'M') c += 13;
+            else if (c >= 'n' && c <= 'z') c -= 13;
+            else if (c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
+       return null;
 
-        return "";
+
     }
-
 }
